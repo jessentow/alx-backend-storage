307744365d25ed20f110bea0838f697c6d79476f
Jesse Ntow <Jessentow@gmail.com>
2024-10-22 22:26:24 -0700

    fix

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""list of collection"""
+"""This will give list of collection"""
 
 
 def schools_by_topic(mongo_collection, topic):
     """
-    returns list of school that having same topic
+    This returns list of school that is having same topic
     """
     return list(mongo_collection.find({"topics": topic}))
